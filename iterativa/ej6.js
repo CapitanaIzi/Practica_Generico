d595a475cc01db55d5bc45acc031552f82829e51
Francesca <guizarfrancesca7@gmail.com>
2024-04-11 22:36:29 -0300

    falta terminar el juego

--- iterativa/ej6.js
+++ iterativa/ej6.js
@@ -1,5 +1,6 @@
 const leer = require("prompt-sync")();
-//un juego donde hay 10 intentos para intentar adivinar el número secreto del programa, si el usuario adivina entonces el programa termina caso contrario al quedarse sin intentos el programa avisa la derrota del usuario
+//un juego donde hay 10 intentos para intentar adivinar el número secreto del programa, si el usuario adivina entonces el programa termina 
+//caso contrario al quedarse sin intentos el programa avisa la derrota del usuario
 let numeroSecreto = "7"
 for (let index = 0; index < 10; index++) {
     console.log("Adivina el nro secreto,son 10 intentos");
---
+++ iterativa/ej7.js
@@ -0,0 +1,25 @@
+const leer = require("prompt-sync")();
+//Crear un juego para adivinar el número secreto generado aleatoriamente en un rango del cero al doble de la cantidad de intentos que desea tener el usuario
+// considerar que la cantidad mínima de intentos es 3 por ende el rango mínimo del número secreto es de 0 a 6
+let numeroSecreto = "7"
+let numIngresado
+let cantIntentos
+console.log("Bienvenido al juego, intenta adivinar el NUMERO que ha generado el programa, pero antes escribe cuantos intentos quieres tener, tiene que ser mas de 3 intentos");
+cantIntentos = leer();
+for (let i = 0; i < cantIntentos; i++) {
+    console.log("Adivina el nro secreto!");
+    numIngresado = Number(leer());
+    if (numIngresado == numeroSecreto) {
+        console.log("Ganaste!!!");
+        i = cantIntentos + 1
+    } else {
+
+        console.log("Sigue intentando, te queda", cantIntentos - 1 - i, "intentos");
+    }
+}
+
+if (numIngresado == numeroSecreto) {
+    console.log("Felicidades :D");
+} else {
+    console.log("Game over :c");
+}
